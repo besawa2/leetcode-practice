8c6c818a9816c07253aa04d4d823f30222f1474c
besawa2 <besawa2@southernct.edu>
2025-09-06 00:08:56 -0400

    LC 559

---
+++ MaxDepthOfNAryTree.py
@@ -0,0 +1,16 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if root is None:
+            return 0
+        if not root.children:
+            return 1
+        return 1 + max(self.maxDepth(child) for child in root.children)
+        
